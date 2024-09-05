15cf7dc7c7121a9c637c82036c2d7fd0133d852f
francivaldo4334 <francivaldodev@gmail.com>
2024-09-05 10:24:43 -0300

    :recycle: refactor component table

--- PowerFitControl/core/src/main/kotlin/br/com/pwrftctrl/core/presenter/ui/components/Table.kt
+++ PowerFitControl/core/src/main/kotlin/br/com/pwrftctrl/core/presenter/ui/components/Table.kt
@@ -14,6 +14,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.Color
 import br.com.pwrftctrl.core.presenter.ui.theme.LocalExtendedColors
+import br.com.pwrftctrl.core.presenter.ui.components.RowCell
 import br.com.pwrftctrl.core.utils.R
 
 data class HeaderItem(
@@ -35,14 +38,18 @@ data class HeaderItem(
 
 
 @Composable
-private fun RowScope.HeaderItemLayout(
-    props: HeaderItem
+inline private fun RowScope.HeaderItemLayout(
+    props: HeaderItem,
+    crossinline onWidthItem: (Dp) -> Unit
 ) {
     var sorted by remember {
         mutableStateOf(false)
     }
     Row(
         modifier = Modifier
+            .drawBehind {
+                onWidthItem(size.width.toInt().dp)
+            }
             .clip(RoundedCornerShape(8.dp))
             .clickable(props.onSortAction != null) {
                 sorted = !sorted
@@ -64,7 +71,7 @@ private fun RowScope.HeaderItemLayout(
                 else Modifier
             )
             .heightIn(min = 32.dp)
-            .padding(horizontal = 12.dp, vertical = 8.dp),
+            .padding(horizontal = 12.dp, vertical = 8.dp) ,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -86,7 +93,10 @@ private fun RowScope.HeaderItemLayout(
 }
 
 @Composable
-private fun Header(items: List<HeaderItem>) {
+private fun Header(
+    items: List<HeaderItem>,
+    onChangeItemSize: (index: Int, width: Dp) -> Unit
+) {
     val extendedColors = LocalExtendedColors.current
     Row(
         modifier =
@@ -98,7 +108,14 @@ private fun Header(items: List<HeaderItem>) {
             .background(extendedColors.secondary100),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        items.forEach { HeaderItemLayout(it) }
+        items.forEachIndexed { index, it -> 
+            HeaderItemLayout(
+                props = it,
+                onWidthItem = { width ->
+                    onChangeItemSize(index, width)
+                }
+            )
+        }
     }
 }
 
@@ -114,37 +131,30 @@ class TableRows(val sizeRow: Int) {
 }
 
 @Composable
-private fun RowScope.CellLayout(props: HeaderItem, content: @Composable () -> Unit) {
-    var cellWindth by remember {
-        mutableStateOf(0)
-    }
+private fun RowScope.CellLayout(width: Dp,content: @Composable () -> Unit) {
     Box(
         modifier = Modifier
-            .then(
-                if (props.maxWidth == null)
-                    Modifier.weight(1f)
-                else Modifier
-            )
-            .then(
-                if (props.minWidth != null)
-                    Modifier.widthIn(min = props.minWidth)
-                else Modifier
-            )
-            .then(
-                if (props.maxWidth != null)
-                    Modifier.widthIn(max = props.maxWidth)
-                else Modifier
-            )
-            .heightIn(min = 32.dp)
-            .drawBehind {
-                cellWindth = size.width.toInt()
-            },
+            .heightIn(min = 32.dp),
         contentAlignment = Alignment.CenterStart
         ) {
         Box(
-            modifier = Modifier.width(cellWindth.dp),
+            modifier = Modifier.width(width),
         ){
             content()
+        }
+    }
+}
+
+@Composable
+fun RowCell(cell: TableRows.Cell, listItemSizes: List<Dp>) {
+    Row(
+        modifier = Modifier
+            .clip(RoundedCornerShape(12.dp))
+            .clickable{ cell.onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        listItemSizes.forEachIndexed { index, it ->
+            CellLayout(it, cell.contents[index])
         }
     }
 }
@@ -158,6 +168,12 @@ fun ColumnScope.Table(
     val tableRows = TableRows(headerItems.size).apply{
         rows() 
     }
+    var widthItems by remember{
+        mutableStateOf(emptyList<Dp>())
+    }
+    var listWidth = remember {
+        mutableStateListOf(*headerItems.map{0.dp}.toTypedArray())
+    }
     Column(
         modifier =
         Modifier.border(2.dp, extendedColors.secondary200, RoundedCornerShape(12.dp))
@@ -165,7 +181,12 @@ fun ColumnScope.Table(
             .fillMaxWidth()
             .padding(12.dp),
     ) { 
-        Header(headerItems)
+        Header(
+            items = headerItems,
+            onChangeItemSize = { id, it ->
+                listWidth[id] = it
+            }
+        )
         LazyColumn(
             modifier = Modifier.fillMaxWidth()
         ){
@@ -173,16 +194,7 @@ fun ColumnScope.Table(
                 items = tableRows.rows,
                 key = { it.key }
             ) { cell -> 
-                Row(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(12.dp))
-                        .clickable{ cell.onClick() },
-                    verticalAlignment = Alignment.CenterVertically
-                ){
-                    headerItems.forEachIndexed { index, it ->
-                        CellLayout(it, cell.contents[index])
-                    }
-                }
+                RowCell(cell, listWidth)
             } 
         }
     }
