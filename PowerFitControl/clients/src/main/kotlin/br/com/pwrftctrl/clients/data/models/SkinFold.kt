94f6ba4711d7e860aadb110bfb4c5595de571f54
francivaldo4334 <francivaldodev@gmail.com>
2024-09-11 11:18:45 -0300

    :sparkles: feat: add models params for physical assessment

--- PowerFitControl/clients/src/main/kotlin/br/com/pwrftctrl/clients/data/models/SkinFold.kt
+++ PowerFitControl/clients/src/main/kotlin/br/com/pwrftctrl/clients/data/models/SkinFold.kt
@@ -4,4 +4,15 @@ import org.jetbrains.exposed.sql.Table
 
 object SkinFold: Table() {
   val id = integer("id").autoIncrement().entityId()
+  val subscapularis = double("subscapularis")
+  val triceps = double("triceps")
+  val biceps = double("biceps")
+  val pectoral = double("pectoral")
+  val obliqueMiddleArmpit = double("oblique_middle_armpit")
+  val verticalMiddleArmpit = double("verticalMiddleArmpit")
+  val horizontalAbdominal = double("horizontal_abdominal")
+  val verticalAbdominal = double("vertical_abdominal")
+  val obliqueSuprailiac = double("oblique_suprailiac")
+  val verticalSuprailiac = double("vertical_suprailiac")
+  val supraexpinal = double("supraexpinal")
 }
