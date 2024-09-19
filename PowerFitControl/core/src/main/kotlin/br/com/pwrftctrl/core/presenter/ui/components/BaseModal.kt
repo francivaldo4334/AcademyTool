76763a54203b238bd4d959277978aa2a437e2a6d
francivaldo4334 <francivaldodev@gmail.com>
2024-09-19 10:50:55 -0300

    :sparkles: feat: add new component base of modal

---
+++ PowerFitControl/core/src/main/kotlin/br/com/pwrftctrl/core/presenter/ui/components/BaseModal.kt
@@ -0,0 +1,36 @@
+package br.com.pwrftctrl.core.presenter.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+@Composable
+fun BaseModal(
+  onDismissRequest: () -> Unit,
+  content: @Composable () -> Unit,
+) {
+  Dialog(
+    onDismissRequest = onDismissRequest,
+    properties = DialogProperties(
+      dismissOnBackPress = false,
+      dismissOnClickOutside = false
+    ),
+  ) {
+    Box(
+      modifier = Modifier
+      .background(color = MaterialTheme.colors.background, shape = RoundedCornerShape(24.dp))
+      .widthIn(min=300.dp)
+      .heightIn(min=200.dp),
+    ){
+      content()
+    }
+}
