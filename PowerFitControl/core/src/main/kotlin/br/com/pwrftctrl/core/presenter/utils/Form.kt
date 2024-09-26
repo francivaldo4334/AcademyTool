ab90a29fe2bd7eaba9c9567ecda15d4bb2edc586
FRANCIVALDO COSTA REIS <21020002@sempreuninassau.com.br>
2024-09-26 19:37:18 -0300

    :recycle: refactor: form class

--- PowerFitControl/core/src/main/kotlin/br/com/pwrftctrl/core/presenter/utils/Form.kt
+++ PowerFitControl/core/src/main/kotlin/br/com/pwrftctrl/core/presenter/utils/Form.kt
@@ -1,67 +1,63 @@
 package br.com.pwrftctrl.core.presenter.utils
 
-import kotlin.collections.MutableMap
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import com.sun.source.tree.IfTree
 
-interface IForm  {
-  val fields: Map<String, MutableState<String>>
-  var messageErrors: MutableMap<String, String>
-  fun getField(fieldName: String): MutableState<String>?{
-    if (fields.contains(fieldName)) {
-      return fields.get(fieldName)
-    }
-    return null
-  }
-  fun getMessageError(fieldName: String): String? {
-    if (messageErrors.contains(fieldName)) {
-      return messageErrors.get(fieldName)
-    }
-    return null
-  }
-  fun validateField(fieldName:String): String?
-  fun valid(): Boolean {
-    var result = true
-    for ((key, value) in fields) {
-      val fieldValue = value.value
-      val errorMessage = validateField(key)
-      if (errorMessage != null) {
-        messageErrors[key] = errorMessage
-        result = false
-      }
-    }
-    return result
-  }
-  fun onSubmit() {
-    if(valid()){
-      onAction()
-      cleanErrors()
-    }
-  }
-  fun cleanErrors() {
-    messageErrors = mutableMapOf<String, String>()
-  }
-  fun onAction()
+fun loadFields(vararg fieldName: String): Map<String, MutableState<String>> {
+    return mapOf(
+        *fieldName.map { it to mutableStateOf("") }.toTypedArray()
+    )
 }
 
-open class Form(
-  override val fields: Map<String, MutableState<String>>,
-  override var messageErrors: MutableMap<String, String> = mutableMapOf()
-) : IForm {
-  private var subform : Form? = null
-  override fun validateField(fieldName: String): String? {
-    TODO("Not yet implemented")
-  }
+open class Form(vararg fieldName: String) {
+    private val fields: Map<String, MutableState<String>> = loadFields(*fieldName)
+    private var errorMessages: MutableMap<String, String> = mutableMapOf()
+    private var forms: MutableMap<String, Form>? = null
+    fun validateField(fieldName: String): String? {
+        TODO("Not yet implemented")
+    }
 
-  override fun onAction() {
-    TODO("Not yet implemented")
-  }
+    fun onAction() {
+        TODO("Not yet implemented")
+    }
 
-}
+    fun getField(fieldName: String): MutableState<String>? {
+        if (fields.contains(fieldName)) {
+            return fields.get(fieldName)
+        }
+        return null
+    }
 
-fun loadFields(vararg fieldName: String) : Map<String, MutableState<String>>{
-  return mapOf(
-    *fieldName.map { it to mutableStateOf("") }.toTypedArray()
-  )
+    fun getErrorMessages(): MutableMap<String, String> {
+        var errorMessages = this.errorMessages
+        this.forms?.forEach { (key, value) ->
+            val valueErrorMessages = value.getErrorMessages()
+            errorMessages += valueErrorMessages
+        }
+        return errorMessages
+    }
+
+    fun validation(): Boolean {
+        var result = true
+        this.fields.forEach{ (key, value) ->
+            val message = validateField(key)
+            if (message != null) {
+                errorMessages[key] = message
+                result = false
+            }
+        }
+        return result
+    }
+
+    fun onSubmit() {
+        if (validation()) {
+            onAction()
+            cleanErrors()
+        }
+    }
+
+    fun cleanErrors() {
+        errorMessages = mutableMapOf()
+    }
+
 }
