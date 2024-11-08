dce90f1943cd1640e01820a13eb11899ca0efbf7
francivaldo4334 <francivaldodev@gmail.com>
2024-11-08 13:52:16 -0300

    :sparkles: feat: add layout make in vuetify

---
+++ vuetify/academy-system/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app theme="light">
+    <v-main>
+      <menu-mdules />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MenuMdules from "./components/MenuMdules.vue"
+export default {
+  name: 'App',
+  components: {
+    MenuMdules,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+<style>
+.v-btn--icon {
+  background: rgb(var(--v-theme-primary));
+}
+
+.v-btn__content {
+  color: rgb(var(--v-theme-background))
+}
+</style>
---
+++ vuetify/academy-system/src/components/MenuMdules.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-navigation-drawer :rail="true" permanent rail-width="56">
+    <v-col class="d-flex flex-column align-center justify-center h-100 pa-0">
+      <v-btn icon :size="40">
+        <Users />
+      </v-btn>
+      <v-btn icon :size="40">
+        <ChartNoAxesCombined />
+      </v-btn>
+      <v-btn icon :size="40">
+        <HandCoins />
+      </v-btn>
+      <v-btn icon :size="40">
+        <Dumbbell />
+      </v-btn>
+    </v-col>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { Users, ChartNoAxesCombined, Dumbbell, HandCoins } from "lucide-vue-next";
+export default {
+  components: {
+    Users, ChartNoAxesCombined, Dumbbell, HandCoins
+  }
+}
+</script>
+<style>
+.v-btn {
+  margin: 8px;
+}
+</style>
