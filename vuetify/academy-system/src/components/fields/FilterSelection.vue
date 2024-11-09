c7f69ed869b0e4965073cea69fa2808895e94883
francivaldo4334 <francivaldodev@gmail.com>
2024-11-09 20:46:14 -0300

    :bug: fix: tooltip filters

--- vuetify/academy-system/src/components/fields/FilterSelection.vue
+++ vuetify/academy-system/src/components/fields/FilterSelection.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="overflow-hidden v-btn--variant-elevated" style="border-radius: 4px; height: 36px;">
     <v-btn v-for="(it,id) in list" :key="id" class="rounded-0" size="36" icon >
-      <v-tooltip :text="$t(it.tip)" location="bottom">
-        <template v-slot:activator="{ props }">
-          <component v-bind="props" :is="it.icon"/>
-        </template>
+      <component v-bind="props" :is="it.icon"/>
+      <v-tooltip activator="parent" location="bottom">
+        {{$t(it.tip)}}
       </v-tooltip>
     </v-btn>
   </div>
