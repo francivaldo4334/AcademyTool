3b99aa7c1afbc3fd7146cc056611c39adb0a0f95
FRANCIVALDO COSTA REIS <21020002@sempreuninassau.com.br>
2024-11-13 19:59:11 -0300

    save

--- vuetify/academy-system/src/components/MenuSideModules.vue
+++ vuetify/academy-system/src/components/MenuSideModules.vue
@@ -1,24 +1,9 @@
 <template>
-  <v-navigation-drawer :rail="true" permanent rail-width="56" class="d-flex flex-column">
-    <v-col class="fill-height d-flex flex-column align-center justify-center pa-0 main-options">
-      <v-btn icon :size="40" :class="getClass('students')" @click="setSelected('students')">
-        <users />
-        <v-tooltip activator="parent" location="end">{{$t('clients')}}</v-tooltip>
-      </v-btn>
-      <v-btn icon :size="40" :class="getClass('metrics')" @click="setSelected('metrics')">
-        <chart-no-axes-combined />
-        <v-tooltip activator="parent" location="end">{{$t('metrics')}}</v-tooltip>
-      </v-btn>
-      <v-btn icon :size="40" :class="getClass('financial')" @click="setSelected('financial')">
-        <hand-coins />
-        <v-tooltip activator="parent" location="end">{{$t('financial')}}</v-tooltip>
-      </v-btn>
-      <v-btn icon :size="40" :class="getClass('equipments')" @click="setSelected('equipments')">
-        <dumbbell />
-        <v-tooltip activator="parent" location="end">{{$t('equipments')}}</v-tooltip>
-      </v-btn>
+  <v-navigation-drawer color="rgb(var(--v-theme-on-background))" :rail="true" permanent rail-width="56" class="d-flex flex-column">
+    <v-col class="fill-height d-flex flex-column align-center justify-center pa-0" style="gap: 16px;">
+      <BtnSelection v-btn v-for="it in options" :key="it.id" :option="it" :isSelected="selected === it.tag" onClick="() => {selected = it.tag}"/>
     </v-col>
-    <v-btn icon :size="40" :class="getClass('settings')" @click="setSelected('settings')" class="position-absolute bottom-0">
+    <v-btn icon :size="40" @click="selected = 'settings'" class="position-absolute bottom-0">
       <Settings />
       <v-tooltip activator="parent" location="end">{{$t('settings')}}</v-tooltip>
     </v-btn>
@@ -34,63 +19,44 @@
   </v-col>
 </template>
 
-<script>
+<script setup lang="ts">
 import { Settings, Users, ChartNoAxesCombined, Dumbbell, HandCoins } from "lucide-vue-next";
 import ClientsScreen from "./clients/MainScreen.vue"
 import MetricsScreen from "./metrics/MainScreen.vue"
 import FinancialScreen from "./financial/MainScreen.vue"
 import EquipmentsScreen from "./equipments/MainScreen.vue"
 import AppBar from "./AppBar.vue"
-
-export default {
-  components: {
-    Users,
-    ChartNoAxesCombined,
-    Dumbbell,
-    HandCoins,
-    Settings,
-    AppBar,
-    ClientsScreen,
-    MetricsScreen,
-    FinancialScreen,
-    EquipmentsScreen,
+import { FunctionalComponent } from "vue"
+import { ref } from "vue"
+import BtnSelection from "./fields/BtnSelection.vue"
+const selected = ref('students')
+type Option = {
+  icon: FunctionalComponent,
+  tag: string,
+  id: number
+}
+const options: Option[] = [
+  {
+    icon: Users,
+    tag: 'students',
+    id: 0
   },
-  data() {
-    return {
-      selected: 'students',
-    }
+  {
+    icon: ChartNoAxesCombined,
+    tag: 'metrics',
+    id: 1
   },
-  methods: {
-    setSelected(icon) {
-      this.selected = icon;
-    },
-    getClass(icon) {
-      return {
-        'selected-btn': this.selected === icon
-      }
-    }
+  {
+    icon: HandCoins,
+    tag: 'financial',
+    id: 2
+  },
+  {
+    icon: Dumbbell,
+    tag: 'equipments',
+    id: 3
   }
-}
+]
 </script>
-<style scoped>
-.v-btn {
-  margin: 8px;
-}
-
-.v-btn--icon {
-  background: rgb(var(--v-theme-on-background)) !important;
-}
-
-.selected-btn {
-  background: rgb(var(--v-theme-primary)) !important;
-  transition: background-color 0.3s ease;
-}
-
-.v-btn--icon:hover {
-  background: rgb(var(--v-theme-surface));
-}
-
-.v-navigation-drawer {
-  background: rgb(var(--v-theme-on-background));
-}
+<style>
 </style>
---
+++ vuetify/academy-system/src/components/fields/BtnSelection.vue
@@ -0,0 +1,31 @@
+<template>
+      <v-btn icon :size="40" @click="onClick"
+             :color="isSelected ? 'primary' : 'transparent'" >
+        <v-icon :icon="option.icon"/> 
+        <v-tooltip activator="parent" location="end">{{$t('clients')}}</v-tooltip>
+      </v-btn>
+</template>
+
+<script lang="ts">
+import { FunctionalComponent } from "vue"
+type Option = {
+  icon: FunctionalComponent,
+  tag: string,
+  id: number
+}
+export default {
+  props: {
+    option: {
+      type: Object as () =>  Option,
+      required: true,
+    },
+    isSelected: {
+      type: Boolean,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style>
+</style>
