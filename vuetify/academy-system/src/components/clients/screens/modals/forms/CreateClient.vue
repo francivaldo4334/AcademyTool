8f37a229920abc615ee2c57d3686a126e7087078
FRANCIVALDO COSTA REIS <21020002@sempreuninassau.com.br>
2024-11-13 19:16:21 -0300

    save

--- vuetify/academy-system/src/components/clients/screens/modals/forms/CreateClient.vue
+++ vuetify/academy-system/src/components/clients/screens/modals/forms/CreateClient.vue
@@ -5,15 +5,15 @@
         <v-text-field variant="outlined" density="compact" :label="$t('name')" hide-details="auto" class="mb-4"
           :prepend-inner-icon="Smile" :rules="[rules.required]" />
         <v-text-field variant="outlined" density="compact" :label="$t('last-name')" hide-details="auto"
-          :prepend-inner-icon="Smile" />
+          :prepend-inner-icon="Smile" :rules="[rules.required]"/>
       </v-col>
       <InputImageFile class="ml-4" />
     </div>
     <v-row class="fill-width pa-0 ma-0">
       <v-text-field variant="outlined" density="compact" :label="$t('cpf')" class="mr-4" hide-details="auto"
-        :prepend-inner-icon="IdCard" placeholder="XX.XXX.XXX-XX" />
+        :prepend-inner-icon="IdCard" placeholder="XX.XXX.XXX-XX" :rules="[rules.required]"/>
       <v-text-field variant="outlined" density="compact" :label="$t('birthday')" hide-details="auto"
-        :prepend-inner-icon="Cake" type="date" />
+        :prepend-inner-icon="Cake" type="date" :rules="[rules.required]"/>
     </v-row>
     <v-text-field variant="outlined" density="compact" :label="$t('e-mail')" hide-details="auto" 
       :prepend-inner-icon="Mail" type="email" :placeholder="$t('example-mail')" />
@@ -32,7 +32,7 @@ import { ref } from "vue"
 import { useI18n } from "vue-i18n"
 const { t } = useI18n()
 const rules = ref({
-  required: (it: string) => !!it || t("required")
+  required: (it: string) => !!it || t("required-field")
 })
 </script>
 <style>
